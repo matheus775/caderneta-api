bd6793f5ed9803fd0e8d3ae6d23feefa8ab4aed0
Matheus Nametala <matheus.nametala@gmail.com>
2021-07-05 10:46:22 -0300

    Projeto criado

---
+++ src/main/java/mathes/nametala/cadernetaapi/CadernetaApiApplication.java
@@ -0,0 +1,13 @@
+package mathes.nametala.cadernetaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadernetaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadernetaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mathes/nametala/cadernetaapi/CadernetaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package mathes.nametala.cadernetaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadernetaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
